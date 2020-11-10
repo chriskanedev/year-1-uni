b394e5e6faceb3291a7e8966fe4cba09d487958d
Chris Kane <christopher_kane@outlook.com>
2020-11-10 11:31:13 +0000

    Fix typo in fibonacci.py
    Fixed the second comment.

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -3,7 +3,7 @@ length = 10
 def fibonacci(n):
     if n == 0:  # 0 is the first Fibonacci number
         return 0
-    elif n==1:  # 0 is the second Fibonacci number
+    elif n==1:  # 1 is the second Fibonacci number
         return 1
 
     return fibonacci(n - 1) + fibonacci(n - 2)
