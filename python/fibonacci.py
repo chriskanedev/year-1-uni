f73a966a6a4a8b1f0f4b851ab791be770f91607e
Chris Kane <christopher_kane@outlook.com>
2020-11-10 11:28:32 +0000

    Create fibonacci.py

---
+++ python/fibonacci.py
@@ -0,0 +1,14 @@
+length = 10
+
+def fibonacci(n):
+    if n == 0:  # 0 is the first Fibonacci number
+        return 0
+    elif n==1:  # 0 is the second Fibonacci number
+        return 1
+
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+# Main
+for i in range(0, length):
+    print("{0} ",fibonacci(i))
