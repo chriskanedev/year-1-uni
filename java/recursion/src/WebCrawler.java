32769cbe51b9820c2e31c60e755e1fb16f768607
Chris Kane <christopher_kane@outlook.com>
2020-11-11 00:29:39 +0000

    Added Java Recursion
    Web and Directory crawler

---
+++ java/recursion/src/WebCrawler.java
@@ -0,0 +1,42 @@
+package com.matt.WebCrawler;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+public class WebCrawler {
+
+	private HashSet<String> links;
+	private static final int MAX_DEPTH = 2;
+
+    public WebCrawler() {
+        links = new HashSet<String>();
+    }
+
+    public void getPageLinks(String url, int depth) {
+        if ((!links.contains(url) && (depth < MAX_DEPTH))) {
+            System.out.println("Depth: " + depth + " [" + url + "]");
+            try {
+                links.add(url);
+                Document document = Jsoup.connect(url).get();
+                Elements pageLinks = document.select("a[href]");
+                depth++;
+                for (Element page : pageLinks) {
+                    getPageLinks(page.attr("abs:href"), depth);
+                }
+            } catch (IOException e) {
+                System.err.println("'" + url + "' caused an error: " + e.getMessage());
+                //e.printStackTrace();
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        new WebCrawler().getPageLinks("http://www.bbc.co.uk/", 0);
+    }
+    
+}
