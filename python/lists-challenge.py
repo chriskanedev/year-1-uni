34c74c531eea5c4f45ea5a19baabde06b619459e
Chris Kane <christopher_kane@outlook.com>
2020-11-17 12:16:01 +0000

    Create lists-challenge.py

---
+++ python/lists-challenge.py
@@ -0,0 +1,14 @@
+lecturers = ["Matt", "Waqar", "Derek"]
+
+# Pop
+print(lecturers.pop(0), lecturers)
+
+# Membership test
+print("Waqar" in lecturers)
+
+# Finding elements
+print(lecturers.index("Derek"))
+
+# Sort
+print(sorted(lecturers))
+print(sorted(lecturers, reverse=True))
